921b25b2796e17f3be5779696b89ec23d97c873e
Pratik Humane <humane.pratik1994@gmail.com>
2022-05-01 15:58:41 +0530

    remove admin-server folder from react app

--- admin-server/validations/resetPasswordValidation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const schema = Joi.object({
-  password: Joi.string().min(6).required(),
-  verification_code: Joi.string(),
-  c_password: Joi.ref("password"),
-});
-
-module.exports = schema;
